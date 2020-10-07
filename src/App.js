c399f5dc3233b208edcf8a71b5267e3a5bf4a616
Janice Asmbrd <jnasmbrd@gmail.com>
2020-10-07 14:10:58 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This is{" "}
-        <a
-          href="https://github.com/jnasmbrd/react-weatherapp-finalproject.git"
-          target="_blank"
-        >
-          open source code
-        </a>{" "}
-        by{" "}
-        <a href="" target="_blank">
-          Janice Asumbrado.
-        </a>
-      </footer>
+      <div className="Container">
+        <h1>Weather App</h1>
+        <footer>
+          This is{" "}
+          <a
+            href="https://github.com/jnasmbrd/react-weatherapp-finalproject.git"
+            target="_blank"
+          >
+            open source code
+          </a>{" "}
+          by{" "}
+          <a href="" target="_blank">
+            Janice Asumbrado.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
