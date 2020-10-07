b633c124685dfb2e59754903cc3d046457ddabff
Janice Asmbrd <jnasmbrd@gmail.com>
2020-10-07 13:57:56 -0600

    Added the Git repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This is{" "}
+        <a
+          href="https://github.com/jnasmbrd/react-weatherapp-finalproject.git"
+          target="_blank"
+        >
+          open source code
+        </a>{" "}
+        by{" "}
+        <a href="" target="_blank">
+          Janice Asumbrado.
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
